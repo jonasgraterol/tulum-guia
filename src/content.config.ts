1d1ce3afe49c58620dbb290a12aec90e2ee460c1
Jonas Graterol <jonas.graterol@versapay.com>
2026-05-03 18:18:57 -0600

    fix: migrate content config to Astro 6 glob loader format

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const places = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.md', base: './src/content/places' }),
   schema: z.object({
     name: z.string(),
     nameEn: z.string(),
